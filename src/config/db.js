9da4a1d50bd4cc05a12555f5b0f5787d2dd0afb7
Bunyodsf <abdurasulovbunyod56@gmail.com>
2025-03-24 16:07:57 +0500

    connection made

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const db = new Sequelize("lc", "root", "1234", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+async function connectDb() {
+  try {
+    await db.authenticate();
+    console.log("connected to db");
+    await db.sync({ force: true });
+    console.log("db synced");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = { db, connectDb, DataTypes };
